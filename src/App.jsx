4be3c3ecf78110c7520c952b53ab0a29182eafd1
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-06-06 16:55:49 -0300

    Commit inicial, projeoto Reactjs SPA

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import {Post} from "./Post"
+
+
+export function App() {
+  return (
+    <>
+    <Post author="Jander Doidao"
+    content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis dolorum, repudiandae consequuntur blanditiis culpa ex eius quidem laboriosam quibusdam totam? A perspiciatis ducimus consectetur placeat atque corporis maxime magni explicabo."
+    />
+    <Post author="Jander Malandro"
+    content="um novo post"
+    />
+    </>
+  )
+}
+
+
---
+++ src/Post.jsx
@@ -0,0 +1,9 @@
+export function Post(props) {
+    return (
+    <>
+        <strong>{props.author}</strong>
+        <p>{props.content}</p>
+    </>    
+    )
+}
+
