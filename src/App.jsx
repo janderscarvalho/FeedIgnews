89e3b1b2e7cdfa3b3d21869c2d7cb4e9c2488654
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-06-12 17:04:43 -0300

    Crialçao Header e Sidebar Ignews

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,30 @@
 import {Post} from "./Post"
+import { Header } from "./components/Header"
 
+import './global.css';
+import styles from './App.module.css';
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
-    <>
-    <Post author="Jander Doidao"
-    content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis dolorum, repudiandae consequuntur blanditiis culpa ex eius quidem laboriosam quibusdam totam? A perspiciatis ducimus consectetur placeat atque corporis maxime magni explicabo."
-    />
-    <Post author="Jander Malandro"
-    content="um novo post"
-    />
-    </>
+    <div>
+    <Header/>
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+          <Post author="Jander Doidao"
+          content="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nobis dolorum, repudiandae consequuntur blanditiis culpa ex eius quidem laboriosam quibusdam totam? A perspiciatis ducimus consectetur placeat atque corporis maxime magni explicabo."
+          />
+          <Post author="Jander Malandro"
+          content="um novo post"
+          />
+      </main>
+
+    </div>
+
+
+    </div>
   )
 }
 
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,23 @@
+import styles from './Sidebar.module.css';
+
+export function Sidebar () {
+    return (
+        <aside className={styles.sidebar}>
+            <img className={styles.cover}
+                src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50" alt="" 
+            />
+
+            <div className={styles.profile}>
+                <strong>Jander Stival</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer className={styles.sidebar}>
+                <a href='#'>
+                    Editar seu Perfil
+                </a>
+            </footer>
+        </aside>
+        
+    );
+}
