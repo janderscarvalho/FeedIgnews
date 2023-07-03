ffe581f7347a7adfd636a668f5af4abdf947dc57
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-07-03 18:06:55 -0300

    Criação de func. Aplaudir

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,20 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { set } from 'date-fns';
+import { useState } from 'react';
 
 export function Comment ({content, onDeleteComment }) {
+    const [likeCount, setLikeCount] = useState(0);
 
     function handleDeleteComment() {
-        onDeleteComment(content);
+        setLikeCount(likeCount + 1);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
     }
      
     return (
@@ -30,9 +39,9 @@ export function Comment ({content, onDeleteComment }) {
                 </div>
 
                 <footer>
-                    <button>
+                    <button onClick={handleLikeComment}>
                         <ThumbsUp />
-                        Aplaudir <span>1</span>
+                        Aplaudir <span>{likeCount}</span>
                     </button>
                 </footer>
             </div>
