21332d6436b4308fb95475c8dbb33513329d517c
Jander Stival de Carvalho <janderscarvalho@gmail.com>
2023-07-05 10:17:45 -0300

    Transformando o codigo em Typescript

---
+++ src/components/Comment.tsx
@@ -0,0 +1,57 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+import { set } from 'date-fns';
+import { useState } from 'react';
+
+interface CommentProps {
+    content: string;
+    onDeleteComment: (comment: string) => void;
+}   
+
+export function Comment ({content, onDeleteComment }: CommentProps) {
+    const [likeCount, setLikeCount] = useState(0);
+
+    function handleDeleteComment() {
+        setLikeCount(likeCount + 1);
+    }
+
+    function handleLikeComment() {
+        setLikeCount((state) => {
+            return state + 1;
+        });
+    }
+     
+    return (
+        <div className={styles.comment}> 
+            <Avatar hasBorder={false} src="https://avatars.githubusercontent.com/u/24524909?v=4" alt=""/>
+
+            <div className={styles.commentBox}>
+                <div className={styles.commentContent}>
+                    <header> 
+                        <div className={styles.authorAndTime}>
+                            <strong>Devon</strong>
+                            <time title="11 de maio ás 08:13h" dateTime='2021-05-11T08:13:00'>Cerca de 1h atrás</time>
+                        </div>
+
+                        <button onClick={handleDeleteComment} title="Deletar comentário">
+                            <Trash size={24} weight='fill'/>
+                        </button>
+
+                    </header>
+                    
+                    <p>{content}</p>
+                </div>
+
+                <footer>
+                    <button onClick={handleLikeComment}>
+                        <ThumbsUp />
+                        Aplaudir <span>{likeCount}</span>
+                    </button>
+                </footer>
+            </div>
+
+
+        </div>
+    )
+}
---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,30 @@
+import { PencilLine } from 'phosphor-react'
+
+import styles from './Sidebar.module.css';
+import { Avatar } from './Avatar';
+
+export function Sidebar () {
+    return (
+        <aside className={styles.sidebar}>
+            <img 
+            className={styles.cover}
+            src="https://images.unsplash.com/photo-1461749280684-dccba630e2f6?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=500&q=50"
+            />
+
+
+            <div className={styles.profile}>
+                <Avatar src="https://avatars.githubusercontent.com/u/24524909?v=4"/>
+                <strong>Jander Stival</strong>
+                <span>Web Developer</span>
+            </div>
+
+            <footer className={styles.sidebar}>
+                <a href='#'>
+                    <PencilLine />
+                    Editar seu Perfi
+                </a>
+            </footer>
+        </aside>
+        
+    );
+}
